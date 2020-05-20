41840c52d9d8d3630e8fccc4fdd3fcf939456eab
Osamah <osamah@aldoaiss.de>
2020-05-20 09:56:47 +0200

    fix

--- src/projects.js
+++ src/projects.js
@@ -1,10 +1,11 @@
 export const Paypal = [
-  { id: 8, paypalId: 'UW9V9S3D8YYDG' },
-  { id: 7, paypalId: '2R2YFCRMF5ALS' },
-  { id: 6, paypalId: 'CTEWRTCGDETLU' },
-  { id: 5, paypalId: 'ZNL6H4HEHUSCW' },
-  { id: 2, paypalId: 'BVVB6A4WUPAT6' },
   { id: 1, paypalId: 'S7ZAP9MNV8EGU' },
-  { id: 4, paypalId: 'B9Q5ELTRX5J22' },
+  { id: 2, paypalId: 'BVVB6A4WUPAT6' },
   { id: 3, paypalId: 'T5Q3VBNH8HVCQ' },
+  { id: 4, paypalId: 'B9Q5ELTRX5J22' },
+  { id: 5, paypalId: 'ZNL6H4HEHUSCW' },
+  { id: 6, paypalId: 'CTEWRTCGDETLU' },
+  { id: 7, paypalId: '2R2YFCRMF5ALS' },
+  { id: 8, paypalId: 'UW9V9S3D8YYDG' },
+  { id: 9, paypalId: 'KKT67K9Y42JUN' },
 ];
