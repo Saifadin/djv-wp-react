4fd04b0fd49b96076f62fba5cc24fb079f3935b7
Osamah Aldoaiss <osamah@aldoaiss.de>
2021-04-20 23:43:55 +0400

    Add Pixel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
+import ReactPixel from 'react-facebook-pixel';
 
 import Footer from './components/Footer';
 
@@ -17,6 +18,7 @@ import DontaionFormPage from './views/DonationForm';
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
     if (this.props.location !== prevProps.location) {
+      ReactPixel.pageView();
       window.scrollTo(0, 0);
     }
   }
@@ -28,35 +30,41 @@ class ScrollToTop extends Component {
 
 withRouter(ScrollToTop);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Helmet>
-          <title>DJV e.V. | Ihre Spende kann vieles Bewirken</title>
-          <meta property="robots" content="index,follow" />
-          <meta property="keywords" content="Charity,Hilfsorganisation,Spenden,Jemen" />
-          <meta property="og:type" content="website" />
-          <meta property="og:locale" content="de_DE" />
-          <meta property="og:site_name" content="DJV e.V." />
-        </Helmet>
-        <Switch>
-          <ScrollToTop>
-            <Route exact path="/" component={Home} />
-            <Route path="/projekte" component={Projects} />
-            <Route path="/ramadan" component={Ramadan} />
-            <Route path="/spenden" component={Donate} />
-            <Route path="/ueber-uns" component={AboutUs} />
-            <Route path="/projekt/:id" component={Project} />
-            <Route path="/impressum" component={Impressum} />
-            <Route path="/danke" component={ThankYou} />
-            <Route path="/spendenbescheinigung" component={DontaionFormPage} />
-          </ScrollToTop>
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => {
+  useEffect(() => {
+    const options = {
+      autoConfig: true,
+      debug: false,
+    };
+    ReactPixel.init('351579262228846', null, options);
+  }, []);
+
+  return (
+    <div className="App">
+      <Helmet>
+        <title>DJV e.V. | Ihre Spende kann vieles Bewirken</title>
+        <meta property="robots" content="index,follow" />
+        <meta property="keywords" content="Charity,Hilfsorganisation,Spenden,Jemen" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="de_DE" />
+        <meta property="og:site_name" content="DJV e.V." />
+      </Helmet>
+      <Switch>
+        <ScrollToTop>
+          <Route exact path="/" component={Home} />
+          <Route path="/projekte" component={Projects} />
+          <Route path="/ramadan" component={Ramadan} />
+          <Route path="/spenden" component={Donate} />
+          <Route path="/ueber-uns" component={AboutUs} />
+          <Route path="/projekt/:id" component={Project} />
+          <Route path="/impressum" component={Impressum} />
+          <Route path="/danke" component={ThankYou} />
+          <Route path="/spendenbescheinigung" component={DontaionFormPage} />
+        </ScrollToTop>
+      </Switch>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
